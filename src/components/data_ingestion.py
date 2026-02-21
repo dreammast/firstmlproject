3212137546a7614b783896dff73b994ae5e52977
dreammast <dreammasterorigin@gmail.com>
2026-02-21 10:36:58 +0530

    model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.model_trainer import ModelTrainer,ModelTrainerConfig,ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -50,4 +51,9 @@ if  __name__ == "__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    r2_score, best_model = modeltrainer.initiate_model_trainer(train_arr,test_arr)
+    print(f"Best Model: {best_model}")
+    print(f"Accuracy (R2 Score): {r2_score}")
