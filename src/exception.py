16c5ff86d54383aa9a212fa2a172eab6d4972780
dreammast <dreammasterorigin@gmail.com>
2026-02-21 10:09:25 +0530

    transformation

--- src/exception.py
+++ src/exception.py
@@ -2,7 +2,7 @@ import sys
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
-    _,_,exe_tb=error_detail.exe_info()
+    _,_,exe_tb=error_detail.exc_info()
     file_name=exe_tb.tb_frame.f_code.co_filename
     error_message ="Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name,exe_tb.tb_lineno,str(error))
